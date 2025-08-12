b3225e8f446cca57ecc8f31d97bab31e8ed38899
Franklin11111 <abdulovnail6@gmail.com>
2025-08-12 12:57:47 +0400

    LRU cache

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" LIFOCache module that inherits from BaseCache class
+""" LRUCache module that inherits from BaseCache class
 """
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """ Inherits from BaseCaching:
-              - it is a LIFO caching system
+              - it is an LRU caching system
+              - discard the least recently used item
               - references the data are stored (in a
                 dictionary of a parent class)
     """
 
     def __init__(self):
-        """ Initiliaze the class
+        """ Initialize the class
          """
         super().__init__()
-        self.last_added_key = ''
+        self.cache_data_keys = []
 
     def put(self, key, item):
         """ Add an item in the cache. If key or item is None,
@@ -26,24 +27,24 @@ class LIFOCache(BaseCaching):
             pass
         else:
             self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            keys = list(self.cache_data.keys())
-            key_to_remove = keys[len(keys) - 2]
-            if self.last_added_key:
-                del self.cache_data[self.last_added_key]
-                print(f"DISCARD: {self.last_added_key}")
+            if key in self.cache_data_keys:
+                self.cache_data_keys.remove(key)
+                self.cache_data_keys.append(key)
             else:
-                del self.cache_data[key_to_remove]
-                print(f"DISCARD: {key_to_remove}")
-            # self.cache_data[key] = item
-        self.last_added_key = key
+                self.cache_data_keys.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            removed_key = self.cache_data_keys.pop(0)
+            del self.cache_data[removed_key]
+            print(f"DISCARD: {removed_key}")
 
     def get(self, key):
         """ Get an item by key. If key is None or if the key doesn’t
             exist in self.cache_data, return None
         """
         if key in self.cache_data:
+            if key in self.cache_data_keys:
+                self.cache_data_keys.remove(key)
+                self.cache_data_keys.append(key)
             return self.cache_data[key]
         else:
             return None
