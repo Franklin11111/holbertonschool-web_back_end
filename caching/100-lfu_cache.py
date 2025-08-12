a96b86acd7f9319c65810f1a00284e303d70ce36
Franklin11111 <abdulovnail6@gmail.com>
2025-08-12 20:07:30 +0400

    LFU cache

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -37,7 +37,7 @@ class LFUCache(BaseCaching):
                 self.cache_data_obj_sorted.pop(first_key)
                 self.cache_data_obj.pop(first_key)
             del self.cache_data[first_key]
-            print(f"DISCARD: {first_key}")
+            print(f"DISCARD: {first_key}\n")
 
         if key in self.cache_data_obj:
             self.cache_data_obj[key] += 1
@@ -58,3 +58,41 @@ class LFUCache(BaseCaching):
             return self.cache_data[key]
         else:
             return None
+
+
+my_cache = LFUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
+my_cache.put("L", "L")
+my_cache.print_cache()
+my_cache.put("M", "M")
+my_cache.print_cache()
