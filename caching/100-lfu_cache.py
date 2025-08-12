b8e47514181ada071ea104b48fc5e5facedaf985
Franklin11111 <abdulovnail6@gmail.com>
2025-08-12 19:54:27 +0400

    LFU cache

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -29,7 +29,6 @@ class LFUCache(BaseCaching):
         else:
             self.cache_data[key] = item
 
-
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data_obj_sorted))
             self.cache_data_obj[first_key] -= 1
@@ -47,9 +46,6 @@ class LFUCache(BaseCaching):
             self.cache_data_obj[key] = 1
             self.cache_data_obj_sorted[key] = 1
             self.cache_data_obj_sorted = dict(sorted(self.cache_data_obj.items(), key=lambda _item: _item[1]))
-        print(self.cache_data_obj_sorted)
-
-
 
     def get(self, key):
         """ Get an item by key. If key is None or if the key doesn’t
@@ -62,40 +58,3 @@ class LFUCache(BaseCaching):
             return self.cache_data[key]
         else:
             return None
-
-my_cache = LFUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
-my_cache.put("L", "L")
-my_cache.print_cache()
-my_cache.put("M", "M")
-my_cache.print_cache()
