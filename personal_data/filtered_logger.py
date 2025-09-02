f345315b5ca2eb936c78cc5f7b17b3b6f00205dc
Franklin11111 <abdulovnail6@gmail.com>
2025-09-02 15:46:31 +0400

    Regexing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,9 +8,7 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    Function for filtering sensitive information
-    """
+    """Function for filtering sensitive information"""
     pattern_regex = '(' + '=.*|'.join(fields) + '=.*)'
     new_message = re.sub(pattern_regex, fields[0]+"="+redaction+separator+fields[1]+"="+redaction+separator, message)
     return new_message
