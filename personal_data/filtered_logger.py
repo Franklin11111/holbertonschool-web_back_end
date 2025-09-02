2f4da40d9689c1269c5a4e37392aad43db9e6005
Franklin11111 <abdulovnail6@gmail.com>
2025-09-02 18:05:38 +0400

    Regexing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,10 +7,12 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """    Function for filtering sensitive information    """
     new_fields = []
     for m in message.split(separator):
-        new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)', f"{m[0:m.find('=')]}={redaction}", m)) \
+        new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
+                                 f"{m[0:m.find('=')]}={redaction}", m)) \
             if m[0:m.find('=')] in fields else new_fields.append(m)
     return separator.join(new_fields)
