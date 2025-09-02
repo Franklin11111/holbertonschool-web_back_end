27715fb1616910e5bbe898f25a6695fc6954a1e7
Franklin11111 <abdulovnail6@gmail.com>
2025-09-02 17:35:18 +0400

    Regexing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,18 +8,9 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Function for filtering sensitive information"""
-    pattern_regex = '(' + '=.*|'.join(fields) + '=.*)'
-    new_message = re.sub(pattern_regex, fields[0]+"="+redaction+separator+fields[1]+"="+redaction+separator, message)
-    return new_message
-
-# def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-#     """    Function for filtering sensitive information    """
-#     pattern_regex = '(' + '=.*|'.join(fields) + '=.*)'
-#     new_fields = []
-#     for m in message.split(';'):
-#         if m[0:m.find('=')] in fields:
-#             new_fields.append(re.sub(pattern_regex, f"{m[0:m.find('=')]}={redaction}", m))
-#         else:
-#             new_fields.append(m)
-#     return ';'.join(new_fields)
+    """    Function for filtering sensitive information    """
+    new_fields = []
+    for m in message.split(';'):
+        new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)', f"{m[0:m.find('=')]}={redaction}", m)) \
+            if m[0:m.find('=')] in fields else new_fields.append(m)
+    return ';'.join(new_fields)
