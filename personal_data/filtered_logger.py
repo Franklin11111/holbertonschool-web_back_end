6a13f1d2eee308907688a0d06e9d8ba78c8f2880
Franklin11111 <abdulovnail6@gmail.com>
2025-09-02 15:45:23 +0400

    Regexing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,21 +7,21 @@ from typing import List
 import re
 
 
-# def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-#     """
-#     Function for filtering sensitive information
-#     """
-#     pattern_regex = '(' + '=.*|'.join(fields) + ')'
-#     new_message = re.sub(pattern_regex, fields[0]+"="+redaction+separator+fields[1]+"="+redaction+separator, message)
-#     return new_message
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """    Function for filtering sensitive information    """
+    """
+    Function for filtering sensitive information
+    """
     pattern_regex = '(' + '=.*|'.join(fields) + '=.*)'
-    new_fields = []
-    for m in message.split(';'):
-        if m[0:m.find('=')] in fields:
-            new_fields.append(re.sub(pattern_regex, f"{m[0:m.find('=')]}={redaction}", m))
-        else:
-            new_fields.append(m)
-    return ';'.join(new_fields)
+    new_message = re.sub(pattern_regex, fields[0]+"="+redaction+separator+fields[1]+"="+redaction+separator, message)
+    return new_message
+
+# def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+#     """    Function for filtering sensitive information    """
+#     pattern_regex = '(' + '=.*|'.join(fields) + '=.*)'
+#     new_fields = []
+#     for m in message.split(';'):
+#         if m[0:m.find('=')] in fields:
+#             new_fields.append(re.sub(pattern_regex, f"{m[0:m.find('=')]}={redaction}", m))
+#         else:
+#             new_fields.append(m)
+#     return ';'.join(new_fields)
