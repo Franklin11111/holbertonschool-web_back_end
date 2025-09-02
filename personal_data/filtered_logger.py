73cac03518bdc8cb91569ee19ccefef7cb5a3bb6
Franklin11111 <abdulovnail6@gmail.com>
2025-09-02 14:29:06 +0400

    Regexing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -12,5 +12,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     Function for filtering sensitive information
     """
     pattern_regex = '(' + '=.*|'.join(fields) + ')'
-    new_message = re.sub(pattern_regex, fields[0]+"="+redaction+separator+fields[1]+"="+redaction, message)
+    new_message = re.sub(pattern_regex, fields[0]+"="+redaction+separator+fields[1]+"="+redaction+separator, message)
     return new_message
