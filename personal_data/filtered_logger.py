ea1d710073f39789e1ebfc3646c7d8b1e0b57be1
Franklin11111 <abdulovnail6@gmail.com>
2025-09-03 11:31:03 +0400

    Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,17 +2,39 @@
 """
 Using Regex for obfuscating sensitive information
 """
-
+from dataclasses import fields
 from typing import List
 import re
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """    Function for filtering sensitive information    """
-    new_fields = []
-    for m in message.split(separator):
-        new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
-                                 f"{m[0:m.find('=')]}={redaction}", m)) \
-            if m[0:m.find('=')] in fields else new_fields.append(m)
-    return separator.join(new_fields)
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = self.filter_datum(fields=["email", "ssn", "password"], redaction=self.REDACTION,
+                                    message=record.msg, separator=self.SEPARATOR)
+        logging.basicConfig(format=self.FORMAT)
+        logger = logging.getLogger(record.name)
+        logger.setLevel(record.levelname)
+        return logger.info(message)
+
+
+    def filter_datum(self, fields: List[str], redaction: str, message: str,
+                     separator: str) -> str:
+        """    Function for filtering sensitive information    """
+        new_fields = []
+        for m in message.split(separator):
+            new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
+                                     f"{m[0:m.find('=')]}={redaction}", m)) \
+                if m[0:m.find('=')] in fields else new_fields.append(m)
+        return separator.join(new_fields)
