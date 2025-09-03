86b39fc8754cdfb74e97ebb3bc752cf4d553a56b
Franklin11111 <abdulovnail6@gmail.com>
2025-09-03 13:37:01 +0400

    Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -23,16 +23,7 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         message = self.filter_datum(self.fields, redaction=self.REDACTION,
                                     message=record.msg, separator=self.SEPARATOR)
-        logging.basicConfig(format=self.FORMAT)
-        logger = logging.getLogger(record.name)
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(record.levelname)
-        formatter = logging.Formatter(self.FORMAT)
-        handler.setFormatter(formatter)
-        logger.setLevel(record.levelname)
-        #logger.addHandler(handler)
-        logger.log(msg=message, level=record.levelno)
-        return ''
+        return message
 
     def filter_datum(self, fields: List[str], redaction: str, message: str,
                      separator: str) -> str:
