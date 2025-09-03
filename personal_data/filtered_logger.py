ec5180417adeb1023126de0fc70f54b5a5af5e3c
Franklin11111 <abdulovnail6@gmail.com>
2025-09-03 19:47:21 +0400

    Read and Filter data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -67,3 +67,19 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=os.getenv('PERSONAL_DATA_DB_NAME')
     )
     return connection
+
+
+def main():
+    db_connection = get_db()
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        log_record = logging.LogRecord("user_data", logging.INFO, None, None, row, None, None)
+        formatter = RedactingFormatter(PII_FIELDS)
+        print(formatter.format(log_record))
+    cursor.close()
+    db_connection.close()
+
+if __name__ == "__main__":
+    main()
