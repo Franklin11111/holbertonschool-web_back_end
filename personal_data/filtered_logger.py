4825651c2dd120b1b68407b77c19ec8f4681c143
Franklin11111 <abdulovnail6@gmail.com>
2025-09-04 10:20:33 +0400

    Check valid password

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -11,6 +11,17 @@ import os
 import mysql.connector
 
 
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """    Function for filtering sensitive information    """
+    new_fields = []
+    for m in message.split(separator):
+        new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
+                                 f"{m[0:m.find('=')]}={redaction}", m)) \
+            if m[0:m.find('=')] in fields else new_fields.append(m)
+    return separator.join(new_fields)
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
