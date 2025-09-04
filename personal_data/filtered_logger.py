649ed02ecd6788249f80a02bed71eed4f0128646
Franklin11111 <abdulovnail6@gmail.com>
2025-09-04 10:30:48 +0400

    Check valid password

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -11,48 +11,48 @@ import os
 import mysql.connector
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """    Function for filtering sensitive information    """
-    new_fields = []
-    for m in message.split(separator):
-        new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
-                                 f"{m[0:m.find('=')]}={redaction}", m)) \
-            if m[0:m.find('=')] in fields else new_fields.append(m)
-    return separator.join(new_fields)
+# def filter_datum(fields: List[str], redaction: str, message: str,
+#                  separator: str) -> str:
+#     """    Function for filtering sensitive information    """
+#     new_fields = []
+#     for m in message.split(separator):
+#         new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
+#                                  f"{m[0:m.find('=')]}={redaction}", m)) \
+#             if m[0:m.find('=')] in fields else new_fields.append(m)
+#     return separator.join(new_fields)
 
 
-# class RedactingFormatter(logging.Formatter):
-#     """ Redacting Formatter class
-#         """
-#
-#     REDACTION = "***"
-#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-#     SEPARATOR = ";"
-#
-#     def __init__(self, fields: List[str]):
-#         super(RedactingFormatter, self).__init__(self.FORMAT)
-#         self.fields = fields
-#
-#     def format(self, record: logging.LogRecord) -> str:
-#         """    Function for formating filtered information    """
-#         message = self.filter_datum(self.fields, redaction=self.REDACTION,
-#                                     message=record.msg,
-#                                     separator=self.SEPARATOR)
-#         return (f"[HOLBERTON] {record.name} {record.levelname} "
-#                 f"{date.today()}-15s:"
-#                 f" {message}")
-#
-#     def filter_datum(self, fields: List[str], redaction: str, message: str,
-#                      separator: str) -> str:
-#         """    Function for filtering sensitive information    """
-#         new_fields = []
-#         for m in message.split(separator):
-#             new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
-#                                      f"{m[0:m.find('=')]}={redaction}", m)) \
-#                 if m[0:m.find('=')] in fields else new_fields.append(m)
-#         return separator.join(new_fields)
-#
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """    Function for formating filtered information    """
+        message = self.filter_datum(self.fields, redaction=self.REDACTION,
+                                    message=record.msg,
+                                    separator=self.SEPARATOR)
+        return (f"[HOLBERTON] {record.name} {record.levelname} "
+                f"{date.today()}-15s:"
+                f" {message}")
+
+    def filter_datum(self, fields: List[str], redaction: str, message: str,
+                     separator: str) -> str:
+        """    Function for filtering sensitive information    """
+        new_fields = []
+        for m in message.split(separator):
+            new_fields.append(re.sub('(' + '=.*|'.join(fields) + '=.*)',
+                                     f"{m[0:m.find('=')]}={redaction}", m)) \
+                if m[0:m.find('=')] in fields else new_fields.append(m)
+        return separator.join(new_fields)
+
 #
 # PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 #
