bf0cf55259a19b0724954f5dd3094c2ea2eed827
Franklin11111 <abdulovnail6@gmail.com>
2025-09-08 19:43:42 +0400

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,8 +25,6 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -37,5 +35,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
