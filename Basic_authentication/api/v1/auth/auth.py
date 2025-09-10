9945f9817b1b71360b1743bf90d5031f2cf97b22
Franklin11111 <abdulovnail6@gmail.com>
2025-09-10 08:30:53 +0400

    Auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+    Auth module
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Require authentication
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Check authentication header
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Check current user
+        """
+        return None
