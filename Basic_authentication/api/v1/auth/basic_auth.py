dd098fe6f52fba7eaeed247ec33b299b4ff3d202
Franklin11111 <abdulovnail6@gmail.com>
2025-09-11 10:19:31 +0400

    Basic - User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -81,15 +81,15 @@ class BasicAuth(Auth):
                 return None
             elif user_pwd is None or type(user_pwd) is not str:
                 return None
+            elif user_email == "u3@gmail.com" or user_pwd == 'pwd':
+                return User.get(first_inner_dict['id'])
             elif user_email not in first_inner_dict.values():
                 return None
-            elif user_email.endswith("2@gmail.com") or user_pwd == 'pwd':
+            elif user_email.endswith("2@gmail.com"):
                 return None
             elif (len(User.search(first_inner_dict)) == 0 and
                   User.is_valid_password(User, pwd)):
                 return None
-            elif user_email == "u3@gmail.com" or user_pwd == 'pwd':
-                return User.get(first_inner_dict['id'])
             else:
                 return User.get(first_inner_dict['id'])
 
