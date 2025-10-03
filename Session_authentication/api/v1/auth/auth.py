b52eabccb7614571b010cfb03e1971d269684bbb
Franklin11111 <abdulovnail6@gmail.com>
2025-10-03 17:24:31 +0400

    Session cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 """
 from typing import List, TypeVar
 from flask import request, jsonify
+import os
 
 
 class Auth:
@@ -53,3 +54,13 @@ class Auth:
         Check current user
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns the value of the session cookie from the request
+        """
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+
+        return request.cookies.get(session_name)
