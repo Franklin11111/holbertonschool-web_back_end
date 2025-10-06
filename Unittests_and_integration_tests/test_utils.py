8611b6b4f14efe61b4cba0acae1d9815216b4bb7
Franklin11111 <abdulovnail6@gmail.com>
2025-10-06 11:48:25 +0400

    Parameterize and patch

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -48,3 +48,31 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Class to test decorator memorization"""
+
+    def test_memoize(self) -> None:
+        """Test if decorated method is not called more than once"""
+        class TestClass:
+            """Class to test memorization"""
+            def a_method(self) -> int:
+                """Method returning fixed value"""
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                """Memorized property calling a_method"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test_class = TestClass()
+            mock_method.return_value = 42
+
+            first_call = test_class.a_property
+            second_call = test_class.a_property
+
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mock_method.assert_called_once()
