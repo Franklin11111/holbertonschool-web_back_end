2cd6404c86164df01bc7568e2ecb2e53cb76fa98
Franklin11111 <abdulovnail6@gmail.com>
2025-10-06 12:38:54 +0400

    More patching

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -31,3 +31,27 @@ class TestGithubOrgClient(unittest.TestCase):
             test_obj = GithubOrgClient('foo')
             self.assertEqual(test_obj._public_repos_url,
                              "https://nowhere.com")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """public_repos unit test
+        - list of repos is what you expect from the chosen payload
+        - mocked property and the mocked get_json was called once"""
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            payload = {'login': 'microsoft',
+                       'id': 6154722,
+                       'node_id': 'MDEyOk9yZ2FuaXphdGlvbjYxNTQ3MjI=',
+                       'url': 'https://api.github.com/orgs/microsoft',
+                       'repos_url':
+                           'https://api.github.com/orgs/microsoft/repos'}
+
+            test_object = GithubOrgClient('foo')
+            mock_get_json.return_value = payload
+            org = test_object.org
+            mock_public_repos_url.return_value = org.get('repos_url')
+
+            self.assertEqual(test_object._public_repos_url,
+                             'https://api.github.com/orgs/microsoft/repos')
+            mock_get_json.assert_called_once()
+            mock_public_repos_url.assert_called_once_with()
