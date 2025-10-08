2af33fa64688b72b08b130e93629cee20ca6e9c2
Franklin11111 <abdulovnail6@gmail.com>
2025-10-08 12:16:00 +0400

    create stored procedure to compute average score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,13 @@
 -- Procedure ComputeAverageScoreForUser that computes average score
 
-DELIMITER //
-
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT           -- user ID
-)
+    IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-
-    -- Calculate mean score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update mean score of the user
-    UPDATE users
-    SET average_score = avg_score
-    WHERE users.id = user_id;
-END;
-//
-
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
