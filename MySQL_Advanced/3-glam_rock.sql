da65e53c5a50dba8aa709678f3332f910dda4508
Franklin11111 <abdulovnail6@gmail.com>
2025-10-08 11:31:15 +0400

    Calculate glam rock ranked by their longevity

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,9 @@
--- List all bands with Glam rock as their main style, ranked by their longevity
+-- List all bands with Glam rock as their main style, 
+-- ranked by their longevity
 
 -- Collect groups of Glam rock and calculate their longevity
-SELECT band_name, 
-       (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan -- Calculate longevity in years
+SELECT band_name, -- Calculate longevity
+       (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan 
 FROM metal_bands -- Metal bands table
 WHERE style LIKE '%Glam rock%' -- Filter by style like Glam rock
 ORDER BY lifespan DESC; -- Order by longevity
